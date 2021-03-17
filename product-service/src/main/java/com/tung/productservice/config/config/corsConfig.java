821a2390f3ead77fa4edbe0630d9d922c6a5ba19
KMS\tungntdo <tungntdo@kms-technology.com>
2021-03-17 10:02:41 +0700

    Add config CORS : disable

---
+++ product-service/src/main/java/com/tung/productservice/config/config/corsConfig.java
@@ -0,0 +1,14 @@
+package com.tung.productservice.config.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class corsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("*");
+    }
+}
