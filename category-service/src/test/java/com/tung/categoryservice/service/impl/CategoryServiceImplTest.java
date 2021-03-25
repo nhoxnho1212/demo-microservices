6713d928aeca0cf9aecdb023e70f27e33da79922
KMS\tungntdo <tungntdo@kms-technology.com>
2021-03-25 09:06:19 +0700

    Reconstruct module structure test : category service

--- category-service/src/test/java/com/tung/categoryservice/service/impl/CategoryServiceImplTest.java
+++ category-service/src/test/java/com/tung/categoryservice/service/impl/CategoryServiceImplTest.java
@@ -19,7 +19,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
-class CategoryDtoServiceImplTest {
+class CategoryServiceImplTest {
 
     @InjectMocks
     CategoryService categoryService = new CategoryServiceImpl();
