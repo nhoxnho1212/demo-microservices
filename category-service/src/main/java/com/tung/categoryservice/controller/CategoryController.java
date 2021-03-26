7721e1030fb0f0229e1fa06aea3ef4e09e4c67ca
KMS\tungntdo <tungntdo@kms-technology.com>
2021-03-26 10:34:28 +0700

    Remove ResponseEntity to return result.

--- category-service/src/main/java/com/tung/categoryservice/controller/CategoryController.java
+++ category-service/src/main/java/com/tung/categoryservice/controller/CategoryController.java
@@ -1,13 +1,10 @@
 package com.tung.categoryservice.controller;
 
 import com.tung.categoryservice.dto.CategoryDto;
-import com.tung.categoryservice.payload.response.ApiResponse;
 import com.tung.categoryservice.payload.response.CategoryResponse;
 import com.tung.categoryservice.service.CategoryService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,19 +27,18 @@ public class CategoryController {
     @GetMapping(
             path = "/{categoryId}"
     )
-    public ResponseEntity<ApiResponse> getCategoryById(@PathVariable final String categoryId) {
+    public CategoryResponse getCategoryById(@PathVariable final String categoryId) {
         CategoryResponse categoryResponse = new CategoryResponse();
 
         CategoryDto categoryDto = categoryService.findById(categoryId);
 
         BeanUtils.copyProperties(categoryDto, categoryResponse);
 
-        ApiResponse response = new ApiResponse(Boolean.TRUE, categoryDto);
-        return new ResponseEntity<>(response, HttpStatus.OK);
+        return categoryResponse;
     }
 
     @GetMapping
-    public ResponseEntity<ApiResponse> getAllCategories() {
+    public List<CategoryResponse> getAllCategories() {
         List<CategoryResponse> listCategoriesResponse = new ArrayList<>();
 
         List<CategoryDto> listCategories = categoryService.findAll();
@@ -53,8 +49,7 @@ public class CategoryController {
             listCategoriesResponse.add(categoryResponse);
         }
 
-        ApiResponse response = new ApiResponse(Boolean.TRUE, listCategoriesResponse);
-        return new ResponseEntity<>(response, HttpStatus.OK);
+        return listCategoriesResponse;
     }
 
 }
--- category-service/src/test/java/com/tung/categoryservice/controller/CategoryControllerTest.java
+++ category-service/src/test/java/com/tung/categoryservice/controller/CategoryControllerTest.java
@@ -1,7 +1,6 @@
 package com.tung.categoryservice.controller;
 
 import com.tung.categoryservice.dto.CategoryDto;
-import com.tung.categoryservice.payload.response.ApiResponse;
 import com.tung.categoryservice.payload.response.CategoryResponse;
 import com.tung.categoryservice.service.CategoryService;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,16 +8,12 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -48,16 +43,11 @@ class CategoryControllerTest {
     void testGetCategoryById() {
         when(categoryService.findById(anyString())).thenReturn(category);
 
-        ApiResponse apiResponse = new ApiResponse(Boolean.TRUE, category);
-        ResponseEntity responseEntity = new ResponseEntity<>(apiResponse, HttpStatus.OK);
+        CategoryResponse result = categoryController.getCategoryById(categoryId);
 
-        ResponseEntity result = categoryController.getCategoryById(categoryId);
-        ApiResponse resultApiResponse = (ApiResponse) result.getBody();
-
-        assertNotNull(resultApiResponse.getMessage());
-        assertEquals(result.getStatusCode(), responseEntity.getStatusCode());
-        assertEquals(resultApiResponse.isSuccess(), apiResponse.isSuccess());
-        assertEquals(resultApiResponse.getMessage(), apiResponse.getMessage());
+        assertNotNull(result);
+        assertEquals(result.getName(), category.getName());
+        assertEquals(result.getId(), category.getId());
         verify(categoryService, times(1)).findById(anyString());
 
     }
@@ -66,18 +56,9 @@ class CategoryControllerTest {
     void testGetAllCategories() {
         when(categoryService.findAll()).thenReturn(listCategories);
 
-        ApiResponse apiResponse = new ApiResponse(Boolean.TRUE, category);
-        ResponseEntity responseEntity = new ResponseEntity<>(apiResponse, HttpStatus.OK);
+        List<CategoryResponse> resultListCategoriesResponse = categoryController.getAllCategories();
 
-        ResponseEntity result = categoryController.getAllCategories();
-        ApiResponse resultApiResponse = (ApiResponse) result.getBody();
-        List<CategoryResponse> resultListCategoriesResponse = (List<CategoryResponse>) resultApiResponse.getMessage();
-
-        assertNotNull(resultApiResponse.getMessage());
-        assertEquals(result.getStatusCode(), responseEntity.getStatusCode());
-        assertEquals(resultApiResponse.isSuccess(), apiResponse.isSuccess());
         assertEquals(resultListCategoriesResponse.size(), listCategories.size());
-
         for (int idx = 0; idx < listCategories.size(); idx ++) {
             CategoryResponse resultCategory = resultListCategoriesResponse.get(idx);
 
